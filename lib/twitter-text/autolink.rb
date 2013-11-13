234faf1af1c8e170e83230db55db28630f5ded24
rkiller <rogerkiller@gmail.com>
2013-11-13 11:12:47 -0800

    URL changes

--- lib/twitter-text/autolink.rb
+++ lib/twitter-text/autolink.rb
@@ -17,13 +17,13 @@ module Twitter
     DEFAULT_CASHTAG_CLASS = "tweet-url cashtag".freeze
 
     # Default URL base for auto-linked usernames
-    DEFAULT_USERNAME_URL_BASE = "https://www.hedgesauce.com/users/".freeze
+    DEFAULT_USERNAME_URL_BASE = "https://www.hedgesauce.com/search?search=%user&".freeze
     # Default URL base for auto-linked lists
     DEFAULT_LIST_URL_BASE = "https://www.hedgesauce.com/".freeze
     # Default URL base for auto-linked hashtags
-    DEFAULT_HASHTAG_URL_BASE = "https://www.hedgesauce.com/search?search=hashtag&".freeze
+    DEFAULT_HASHTAG_URL_BASE = "https://www.hedgesauce.com/search?search=%hashtag&".freeze
     # Default URL base for auto-linked cashtags
-    DEFAULT_CASHTAG_URL_BASE = "https://www.hedgesauce.com/search?search=cashtag&".freeze
+    DEFAULT_CASHTAG_URL_BASE = "https://www.hedgesauce.com/search?search=%cashtag&".freeze
 
     # Default attributes for invisible span tag
     DEFAULT_INVISIBLE_TAG_ATTRS = "style='position:absolute;left:-9999px;'".freeze
